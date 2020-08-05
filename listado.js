7044ba90e1a694f1b7237d2a23e7238c1923f3ce
bymahak4 <rodriguezdiego.uy@gmail.com>
2020-08-05 14:25:06 -0300

    Updated listado.js
    Se eliminaron los comentarios

--- listado.js
+++ listado.js
@@ -18,15 +18,11 @@ function showMenuList(array){
     }
 }
 
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){  
     getJSONData(LIST_URL).then(function(resultObj){
         if (resultObj.status === "ok")
         {
             menuArray = resultObj.data;
-            //Muestro las categorías ordenadas
             showMenuList(menuArray);
         }
     });
